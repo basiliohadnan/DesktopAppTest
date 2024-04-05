b2227a3386a224fb5d941865835631c703d573dd
Hadnan Basilio <hadnan.basilio@starline.inf.br>
2024-04-05 13:14:50 -0300

    Modularizing.

---
+++ CalculatorTests/Tests/CalculatorTest.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Windows;
+
+namespace CalculatorTests.Tests
+{
+    public class CalculatorTest
+    {
+        public void RunTest()
+        {
+            // Set up WinAppDriver connection
+            using (WindowsDriver<WindowsElement> calculatorSession = InitializeCalculatorSession())
+            {
+                // Wait for the calculator to load
+                Thread.Sleep(2000);
+
+                // Perform calculation
+                PerformCalculation(calculatorSession);
+
+                // Capture and read the result using OCR
+                string result = OCRHelper.ReadTextFromImage("screenshot.png");
+
+                // Output the result
+                Console.WriteLine("Result: " + result);
+            }
+        }
+
+        private WindowsDriver<WindowsElement> InitializeCalculatorSession()
+        {
+            WindowsDriver<WindowsElement> calculatorSession = null;
+            AppiumOptions appCapabilities = new AppiumOptions();
+            appCapabilities.AddAdditionalCapability("app", "Microsoft.WindowsCalculator_8wekyb3d8bbwe!App");
+            calculatorSession = new WindowsDriver<WindowsElement>(new Uri("http://127.0.0.1:4723"), appCapabilities);
+            return calculatorSession;
+        }
+
+        private void PerformCalculation(WindowsDriver<WindowsElement> calculatorSession)
+        {
+            // Enter '2 + 2' in the calculator
+            calculatorSession.FindElementByAccessibilityId("num2Button").Click();
+            calculatorSession.FindElementByAccessibilityId("plusButton").Click();
+            calculatorSession.FindElementByAccessibilityId("num2Button").Click();
+            calculatorSession.FindElementByAccessibilityId("equalButton").Click();
+        }
+    }
+}
---
+++ CalculatorTests/Tests/OCRHelper.cs
@@ -0,0 +1,21 @@
+﻿using Tesseract;
+
+namespace CalculatorTests.Tests
+{
+    public static class OCRHelper
+    {
+        public static string ReadTextFromImage(string imagePath)
+        {
+            using (var engine = new TesseractEngine(@"C:\Users\Starline\source\repos\CalculatorTests\tessdata", "eng", EngineMode.Default))
+            {
+                using (var img = Pix.LoadFromFile(imagePath))
+                {
+                    using (var page = engine.Process(img))
+                    {
+                        return page.GetText();
+                    }
+                }
+            }
+        }
+    }
+}
