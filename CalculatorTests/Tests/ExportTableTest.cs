999120b1234797fae1bdc930db91ae461bcc1a38
Hadnan Basilio <hadnan.basilio@starline.inf.br>
2024-04-11 12:48:54 -0300

    ExportTable's test working properply;

--- CalculatorTests/Tests/ExportTableTest.cs
+++ CalculatorTests/Tests/ExportTableTest.cs
@@ -1,19 +1,20 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using OpenQA.Selenium;
 using OpenQA.Selenium.Appium;
 using OpenQA.Selenium.Appium.Windows;
-using DesktopApp.Helpers;
 using WindowsInput;
+using System.Threading;
+using DesktopApp.Helpers;
+using OpenQA.Selenium;
 
 namespace DesktopApp.Tests
 {
     [TestClass]
     public class ExportTableTest
     {
-        private const string app = "ExportTable";
-        private const string appPath = $@"C:\\Users\\Starline\\Documents\\AppGrid\{app}.exe";
-        private const string ScreenshotsDirectory = $@"C:\Users\Starline\source\repos\DesktopAppTest\CalculatorTests\Screenshots\{app}";
+        private const string AppPath = @"C:\Users\Starline\Documents\AppGrid\ExportTable.exe";
+        private const string ScreenshotsDirectory = @"C:\Users\Starline\source\repos\DesktopAppTest\CalculatorTests\Screenshots\ExportTable";
         private const string WinAppDriverPath = @"C:\Program Files (x86)\Windows Application Driver\WinAppDriver.exe";
         private static WindowsDriver<WindowsElement> appSession;
 
@@ -21,8 +22,7 @@ namespace DesktopApp.Tests
         public static void ClassInitialize(TestContext context)
         {
             StartWinAppDriver();
-            LaunchTestedApp(appPath);
-            //appSession = InitializeAppSession(appPath);
+            appSession = InitializeAppSession(AppPath);
         }
 
         [ClassCleanup]
@@ -36,26 +36,20 @@ namespace DesktopApp.Tests
         [TestMethod]
         public void ValidatesTextInserted()
         {
-            // Define expected result
-            //string expectedResult = "teste";
-
-            //// Insert values inside the app
-            //WriteTest(expectedResult);
+            string expectedResult = "String1";
 
             // Capture and save screenshot with timestamp
             string screenshotPath = ScreenPrinter.CaptureAndSaveScreenshot(appSession, ScreenshotsDirectory);
 
-            //// Extract result from app using OCR
-            //string textExtracted = OCRTranslator.ExtractText(screenshotPath, 0, 45, 32, 15, 150);
+            string textExtracted = OCRTranslator.ExtractText(screenshotPath, 109, 197 , 68, 15, 165);
 
-            //Assert.AreEqual(expectedResult, textExtracted);
+            Assert.AreEqual(expectedResult, textExtracted);
         }
 
         private static void StartWinAppDriver()
         {
             // Start WinAppDriver process if not already running
-            Process[] processes = Process.GetProcessesByName("WinAppDriver");
-            if (processes.Length == 0)
+            if (Process.GetProcessesByName("WinAppDriver").Length == 0)
             {
                 Process.Start(WinAppDriverPath);
                 // Wait for WinAppDriver to start
@@ -65,44 +59,17 @@ namespace DesktopApp.Tests
 
         private static void StopWinAppDriver()
         {
-            // Stop WinAppDriver process
-            Process[] processes = Process.GetProcessesByName("WinAppDriver");
-            foreach (Process process in processes)
+            // Stop WinAppDriver process if running
+            foreach (Process process in Process.GetProcessesByName("WinAppDriver"))
             {
                 process.Kill();
             }
         }
 
-        private static WindowsDriver<WindowsElement> LaunchTestedApp(string appPath)
-        {
-            Process.Start(appPath);
-
-            // Wait 2sec
-            Thread.Sleep(2000);
-
-            // Press Enter
-            InputSimulator inputSimulator = new InputSimulator();
-            inputSimulator.Keyboard.KeyPress(WindowsInput.Native.VirtualKeyCode.RETURN);
-            Thread.Sleep(2000);
-
-            // Create session at root level
-            AppiumOptions rootCapabilities = new AppiumOptions();
-            rootCapabilities.AddAdditionalCapability("app", "Root");
-            WindowsDriver<WindowsElement> winSession = new WindowsDriver<WindowsElement>(new Uri("http://127.0.0.1:4723"), rootCapabilities);
-
-            // Launch the application using the provided appPath
-            AppiumOptions appCapabilities = new AppiumOptions();
-            appCapabilities.AddAdditionalCapability("app", appPath);
-            WindowsDriver<WindowsElement> appSession = new WindowsDriver<WindowsElement>(new Uri("http://127.0.0.1:4723"), appCapabilities);
-
-            return appSession;
-
-        }
-
         private static void CloseTestedApp()
         {
-            Process[] processes = Process.GetProcessesByName(app.ToLower());
-            foreach (Process process in processes)
+            // Close ExportTable application if running
+            foreach (Process process in Process.GetProcessesByName("ExportTable"))
             {
                 process.Kill();
             }
@@ -110,27 +77,30 @@ namespace DesktopApp.Tests
 
         private static WindowsDriver<WindowsElement> InitializeAppSession(string appPath)
         {
-            // Create session at root level
-            AppiumOptions rootCapabilities = new AppiumOptions();
-            rootCapabilities.AddAdditionalCapability("app", "Root");
-            WindowsDriver<WindowsElement> winSession = new WindowsDriver<WindowsElement>(new Uri("http://127.0.0.1:4723"), rootCapabilities);
+            // Start the app's process
+            var process = Process.Start(appPath);
 
-            // Launch the application using the provided appPath
-            AppiumOptions appCapabilities = new AppiumOptions();
-            appCapabilities.AddAdditionalCapability("app", appPath);
-            WindowsDriver<WindowsElement> appSession = new WindowsDriver<WindowsElement>(new Uri("http://127.0.0.1:4723"), appCapabilities);
-            
-            // Wait 2sec
+            // Wait for a short duration to allow the process to initialize
             Thread.Sleep(2000);
 
-            // Press Enter
+            // Simulate pressing the Enter key
             InputSimulator inputSimulator = new InputSimulator();
             inputSimulator.Keyboard.KeyPress(WindowsInput.Native.VirtualKeyCode.RETURN);
 
-            // Wait 3sec
-            Thread.Sleep(3000);
+            // Wait for another short duration after pressing Enter
+            Thread.Sleep(2000);
 
-            return appSession;
+            // Get the window handle of the ExportTable.exe process
+            IntPtr mainWindowHandle = process.MainWindowHandle;
+
+            // Identify the root level window of the ExportTable.exe process
+            WindowsDriver<WindowsElement> winSession;
+            AppiumOptions rootCapabilities = new AppiumOptions();
+            // Use the window handle as the appTopLevelWindow capability
+            rootCapabilities.AddAdditionalCapability("appTopLevelWindow", mainWindowHandle.ToInt64().ToString("x"));
+            winSession = new WindowsDriver<WindowsElement>(new Uri("http://127.0.0.1:4723"), rootCapabilities);
+
+            return winSession;
         }
 
         private void WriteTest(string text)
