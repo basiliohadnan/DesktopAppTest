9326be5e7724fd6a0349d1c784d543851607d8c4
basiliohadnan <hadnan.basilio@starline.inf.br>
2024-04-15 13:18:39 -0300

    Creating BoundingRectangle structure

--- CalculatorTests/Helpers/ElementHandler.cs
+++ CalculatorTests/Helpers/ElementHandler.cs
@@ -25,5 +25,22 @@ namespace DesktopAppTests.Helpers
             }
             return null;
         }
+
+        public struct BoundingRectangle
+        {
+            public int Left { get; set; }
+            public int Top { get; set; }
+            public int Right { get; set; }
+            public int Bottom { get; set; }
+
+            public BoundingRectangle(int left, int top, int right, int bottom)
+            {
+                Left = left;
+                Top = top;
+                Right = right;
+                Bottom = bottom;
+            }
+        }
+
     }
 }
