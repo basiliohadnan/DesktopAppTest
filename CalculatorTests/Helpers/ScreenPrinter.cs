0548d8fd52e7a6b79e7f013b93e86dc2b14a5529
basiliohadnan <hadnan.basilio@starline.inf.br>
2024-04-18 10:23:11 -0300

    Altering CaptureAndSaveScreenshot to consider testName

--- CalculatorTests/Helpers/ScreenPrinter.cs
+++ CalculatorTests/Helpers/ScreenPrinter.cs
@@ -5,16 +5,17 @@ namespace Consinco.Helpers
 {
     public class ScreenPrinter
     {
-        public static string CaptureAndSaveScreenshot(WindowsDriver<WindowsElement> appSession, string directoryPath)
+        public static string CaptureAndSaveScreenshot(string directoryPath, string testName)
         {
             // Generate filename with timestamp
             string timestamp = DateTime.Now.ToString("yyyyMMddHHmmss");
             string filename = $"screenshot_{timestamp}.png";
 
             // Capture screenshot using ITakesScreenshot interface
-            var screenshot = ((ITakesScreenshot)appSession).GetScreenshot();
+            var screenshot = ((ITakesScreenshot)Global.appSession).GetScreenshot();
 
             // Ensure the directory exists or create it
+            directoryPath = directoryPath + Global.app + "\\" + testName; 
             Directory.CreateDirectory(directoryPath);
 
             // Save screenshot to a file
