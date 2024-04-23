707e6dd9585d31dbc76fdf076fa9a16df9129f24
basiliohadnan <hadnan.basilio@starline.inf.br>
2024-04-23 14:34:10 -0300

    Test login dummy logged with success.

--- CalculatorTests/MaxCompra/MaxCompraInit.cs
+++ CalculatorTests/MaxCompra/MaxCompraInit.cs
@@ -31,11 +31,9 @@ namespace Consinco.MaxCompra
         {
             FillField(user);
             PressEnter();
+            SetAppSession("Centura:MDIFrame");
 
-            ////
             //PressEnter();
-
-            //SetAppSession("Centura:MDIFrame");
             //ScreenPrinter.CaptureAndSaveScreenshot(Global.screenshotsDirectory, "01-Login");
         }
 
@@ -50,45 +48,79 @@ namespace Consinco.MaxCompra
         public void LoginDummy()
         {
             Console.WriteLine("Global Variables");
-            string scenarioName = "Relatorio modelo login";
+            string scenarioName = "Relatorio - Realizar Login";
             int reportID = 1;
             int lgsID;
             string printFileName;
             // name
-            string expectedResult = "Não foi definido a versão do módulo no BANCO DE DADOS, para o sistema de Segurança";
 
             Console.WriteLine("Test Variables");
             string testName = "Login";
             string testType = "Funcional";
             string analystName = "Hadnan Basilio";
-            string testDesc = "Realizar login no MaxCompra com matricula do analista";
+            string testDesc = "Realizar login no app MaxCompra com matricula de analista";
             Global.processTest.StartTest(Global.customerName, suiteName, scenarioName, testName, testType, analystName, testDesc, reportID);
 
             Console.WriteLine("Test Details");
-            string preCondition = "Caixa aberto";
+            string preCondition = "App instalado";
             string postCondition = "Nenhuma";
-            string inputData = "Código de produto válido";
+            string inputData = "Matricula valida";
             Global.processTest.DoTest(preCondition, postCondition, inputData);
 
             Console.WriteLine("Steps Definition");
             Global.processTest.DoStep("Abrir app", "Abertura do app com sucesso");
             Global.processTest.DoStep("Login do analista", "Login com sucesso");
-            Global.processTest.DoStep("Tela final", expectedResult);
+            Global.processTest.DoStep("Tela final", "xx");
 
             Console.WriteLine("Steps Execution");
-
-            lgsID = Global.processTest.StartStep("Login do analista", logMsg: "Tentando login", paramName: "matricula", paramValue: user);
+            lgsID = Global.processTest.StartStep("Abrir app", logMsg: "Tentando abrir app", paramName: "appPath", paramValue: appPath);
             // Step actions
-            Login();
+            // Test Initialize runs
+            // Opens app
+            var windowName = "Conexão de Sistemas Consinco";
+            var welcomeWindow = elementHandler.FindElementByName(windowName);
 
             printFileName = Global.processTest.PrintScreen();
-            if (true)
+            if (welcomeWindow != null)
+            {
+                Global.processTest.EndStep(lgsID, printPath: printFileName, logMsg: "app aberto");
+            }
+            else
+            {
+                Global.processTest.EndStep(lgsID, status: "erro", printPath: printFileName, logMsg: "erro na abertura do app");
+            }
+
+            lgsID = Global.processTest.StartStep("Login do analista", logMsg: "Tentando login", paramName: "matricula", paramValue: user);
+            Login();
+            var warningText = "Não foi definido a versão do módulo no BANCO DE DADOS, para o sistema de Segurança";
+            var databaseWarning = elementHandler.FindElementByXPathPartialName(warningText);
+
+            printFileName = Global.processTest.PrintScreen();
+            if (databaseWarning != null)
             {
                 Global.processTest.EndStep(lgsID, printPath: printFileName, logMsg: "login efetuado");
             }
             else
             {
                 Global.processTest.EndStep(lgsID, status: "erro", printPath: printFileName, logMsg: "erro no login");
+            }
+
+            lgsID = Global.processTest.StartStep("Tela final", logMsg: "Tentando acessar tela principal", paramName: "", paramValue: "");
+            databaseWarning.Click();
+            
+            PressEnter();
+
+            var mainWindowName = "acrux mercari - Compras";
+            var mainWindow = elementHandler.FindElementByXPathPartialName(mainWindowName);
+
+            printFileName = Global.processTest.PrintScreen();
+            if (mainWindow != null)
+            {
+                Global.processTest.EndStep(lgsID, printPath: printFileName, logMsg: "tela principal exibida");
+            }
+            else
+            {
+                Global.processTest.EndStep(lgsID, status: "erro", printPath: printFileName, logMsg: "erro na exibição da tela principal");
             }
 
             Console.WriteLine("Teardown function");
